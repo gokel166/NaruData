aa0ddd3f39450e9adc6551aac1975d3f21104708
gokel166 <okellogeorge40@yahoo.com>
2018-02-08 01:04:33 -0600

    create custom Home table

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,17 +7,10 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'navbar'
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
