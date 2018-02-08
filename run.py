97907e877b857ebb996b38fddd97dc535356f116
gokel166 <okellogeorge40@yahoo.com>
2018-02-08 01:44:35 -0600

    connect front and backend files with cors

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, jsonify
 from random import *
+from flask_cors import CORS
 
 app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/random')
 def random_number():
